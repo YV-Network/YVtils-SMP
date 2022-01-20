ef8e68835d3dc742d7f9a6774e471188ab9618ae
WolfiiYV <minecraftwandler@gmail.com>
2022-01-20 18:27:02 +0100

    New Features:
    GlobalMute -> With /globalmute; /chatmute or /mutechat you can disable the chat for everyone that don't have the Permission "yvtils.ba.bypass.mutechat".
    
    AFKDamageKick -> With /afkdamage you can toggle the automatic kick when you get Damage, but you don't allowed to move to much otherwise you will get kicked and the Setting will disabled. When you get Damage you will get kicked and the Setting get disabled. -> Required Permission: "yvtils.ba.command.afkdamage"
    
    Config has new Options. -> Version 8

---
+++ src/main/java/de/yvtils/ba/utils/LicenseCode.java
@@ -0,0 +1,10 @@
+package de.yvtils.ba.utils;
+
+public class LicenseCode {
+    public static String NORMAL = "y33Yu79v4wXe43wA6c9PJ7qsuhXNj6_NORMALKEY";
+    public static String OLDPREMIUM = "3bB7ah6Epy7Q5xqAjF9b4Y8r7Ft685";
+    public static String PREMIUM = "3bB7ah6Epy7Q5xqAjF9b4Y8r7Ft685_PREMIUMKEY";
+    public static String EVENTPREMIUM = "Bc8D374FMXLf&iT&bd96LbipB8&Koc2sbnssB5i9d786$2ANGa_EVENTPREMIUMKEY";
+    public static String CODING = "zd3d46jR9JURS5t28Y3d3zW7yf3925_CODINGKEY";
+    public static String YVSMP = "q4QdPvvdSYv6j32BqdoMP3eH$Bxtq^*2Zs4k4dLjg7s*3@BYCC5drnx5wsJ3dh@&x74Cp!cZt&h_YVSMP";
+}
